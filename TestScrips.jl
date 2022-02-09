fddfca5dec7a78aed615a62baface775a12031f1
Leo Breston <leo.breston@gmail.com>
2022-02-08 17:02:45 -0800

    updates

--- TestScrips.jl
+++ TestScrips.jl
@@ -1,8 +1,43 @@
 include("./ROIontology.jl")
+using Chain 
 
-ROIexp=load("./Data/ROIexp.jld2", "ROIexp")
+ROIexp = load("./Data/ROIexp.jld2", "ROIexp")
+lfp = ROIexp[:lfp_data]
+events  = ROIexp[:behavioral_events]
 
 
-lfp=ROIexp[:lfp_recordings]
-lfp.region
-lfp.region
+function get_event_lfp_recording(event, lfp)
+    f1(l) = in_interval(event, l) 
+    filter(x -> f1(x), lfp)
+end
+
+get_event_lfp_recording(events[2], lfp)
+
+
+
+time_interval(events[1])
+typeof(events[1]) <: Observation
+ObservationInterval
+BehavioralEvent <: Observation
+events
+# value(lfp[2])
+# dims(lfp[2])
+
+# Table(lfp)
+
+# import DataFrames as df
+
+# LFPdf = df.DataFrame(lfp)
+
+# using DataFramesMeta
+
+# df.groupby(LFPdf, :region)
+
+# transform!(LFPdf, :lfp => ByRow.(x -> spectra(x, 256, 512)) => :spectra)
+
+
+
+# LFPdf.spectra[1]
+
+# using Plots
+# spec = LFPdf.spectra[1:5]
