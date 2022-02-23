eff0c08e7d8bb4a419f062deb3a8571c54845e74
Leo Breston <leo.breston@gmail.com>
2022-02-22 19:37:11 -0800

    phase plots

--- TestPlots.jl
+++ TestPlots.jl
@@ -15,6 +15,7 @@ const WC = collect(codom(window_data))
 using Interpolations
 using CairoMakie
 
+
 dataregion = MOB
 dataevent="Sniff"
 
@@ -41,17 +42,44 @@ for n in 1:9
 end
 f
 
-
+##==============================================================================
 # heatmap(1:.5:3357, .5:.5:60, rotl90(S.y); colormap = :jet)
+using CairoMakie
+using Interpolations
+CairoMakie.activate!(type="png")
 x=inverse(window_data_event)(events[1])
-S=TFanalyticsignal(x[1][1:1000], 256, 0, 1)
-R=TFanalyticsignal(x[2][1:1000], 256, 0, 1)
-using ComplexPhasePortrait
+f= Figure();
+# axs = [Axis(f[i, j]) for i = 1:4, j=1:4]
+for i in 1:4
+        for j in i+1:4
+                S=FA.TFanalyticsignal(x[i], 256, 0, 1)
+                R=FA.TFanalyticsignal(x[j], 256, 0, 1)
+        
+                l= size(S.y, 2)
+                ph = angle.(rotl90(conj(R.y).*(S.y)))[:, 1:128]
+                amp = log10.(abs.(rotl90(conj(R.y).*(S.y)))[:, 1:128])
 
-fig=Figure();
-ax, h = heatmap(fig[1,1],angle.(rotl90(conj(R.y).*(S.y)))[:,1:60]; colormap = :roma, colorrange = (-π,π), backlight = 1f0, highclip = :black, interpolate=true)
-Colorbar(fig[1, 2], h, ticks = ([-π, -π / 2, 0, π / 2, π], String.([-π, -π / 2, 0, π / 2, π])))
-fig
+                itp=interpolate(ph, BSpline(Cubic(Line(OnGrid()))))
+
+                ax, h = heatmap(f[j-1,i], collect((1:1:l)./1010.1), 1:.1:128, itp[1:1:l, 1:.1:128] ; colormap = :romaO		
+                , colorrange = (-π,π), backlight = 1f0)
+                ax.yticks = ([1,64,128], ["1","36", "64"])
+                if i == 1 
+                        ax.ylabel = "Freq (Hz)"
+                end
+                if j == 4
+                        ax.xlabel = "Time (s)"
+                end
+                e=window_data_event(x[1]).behavior.name
+                r1=window_data_region(x[i]).name
+                r2=window_data_region(x[j]).name
+                ax.title = "$r1 × $r2"
+                vlines!(ax, 1; color=:green, linewidth=2, linestyle=:dash)
+                vlines!(ax, l/1010.1-1;color=:red, linewidth=2, linestyle=:dash)
+                # Colorbar(h, ticks = ([-π, -π / 2, 0, π / 2, π], [L"-\pi", L"-\pi/2", L"0", L"\pi/2", L"\pi"]))
+        end
+end
+f
 ##==============================================================================
 # itp=interpolate(rotl90(S.y), BSpline(Cubic(Line(OnGrid()))))
 # x=1:1:size(S.y, 2)
@@ -64,3 +92,41 @@ fig
 # hidedecorations!(ax)
 # f
 #diverging_rainbow_bgymr_45_85_c67_n256, 
+##==============================================================================
+using CairoMakie
+let
+        x = y = -2:0.005:2
+        f(z) = 1 / (z * (z^2 - z - 1 - 3im))
+        fvals = [f(u + 1im * v) for u in x, v in y]
+        fvalues = abs.(fvals)
+        fargs = angle.(fvals)
+        polya(x, y) = Point2f(real(f(x + 1im * y)), -imag(f(x + 1im * y)))
+
+        fig = Figure(resolution = (900, 400))
+        axs = [Axis(fig[1, i], aspect = 1) for i in 1:2]
+        cmap = :roma
+        streamplot!(axs[1], polya, -2 .. 2, -2 .. 2, colormap = (:black, :red),
+                gridsize = (40, 40), arrow_size = 6, linewidth = 1)
+        pltobj2 = heatmap!(axs[2], x, y, fargs, colorrange = (-π, π), colormap = cmap)
+        streamplot!(axs[2], polya, -2 .. 2, -2 .. 2, colormap = (:black, :black),
+                gridsize = (40, 40), arrow_size = 6, linewidth = 1)
+        Colorbar(fig[1, 3], pltobj2, ticks = ([-π, -π / 2, 0, π / 2, π],
+                [L"-\pi", L"-\pi/2", L"0", L"\pi/2", L"\pi"]))
+        limits!(axs[1], -2, 2, -2, 2)
+        limits!(axs[2], -2, 2, -2, 2)
+        colsize!(fig.layout, 1, Aspect(1, 1.0))
+        colsize!(fig.layout, 2, Aspect(1, 1.0))
+        display(fig)
+end
+
+##==============================================================================
+using CairoMakie
+f = Figure(fontsize = 18);
+
+Axis(f[1, 1],
+    title = L"\frac{x + y}{\sin(k^2)}",
+    xlabel = L"\sum_a^b{xy}",
+    ylabel = L"\sqrt{\frac{a}{b}}"
+)
+
+f
