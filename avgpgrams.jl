d64a23b227998407264ab4424518fdebf2eef706
Leo Breston <leo.breston@gmail.com>
2022-03-11 13:56:26 -0800

    new hypos

--- avgpgrams.jl
+++ avgpgrams.jl
@@ -9,7 +9,7 @@ include("./ROIontology.jl")
 ##==============================================================================
 #Load data
 
-ROIexp = load("./Data/ROIexp.jld2", "ROIexp");
+ROIexp = load("./Data/ROIexp_FR.jld2", "ROIexp");
 lfp = ROIexp[:lfp_data]
 events = ROIexp[:behavioral_events]
 trials = ROIexp[:trials]
@@ -44,9 +44,10 @@ using CairoMakie
 include("./PlotFuncs.jl")
 using SplitApplyCombine
 
+
 using Statistics
 
-function fuckyouplot(e, tc, r)
+function fuckyouplot(ax, e, tc, r)
     # dataregion = MOB
     event_name = e
     # agent_types = ["Rat", "Robot", "Object"]
@@ -57,41 +58,65 @@ function fuckyouplot(e, tc, r)
 
     g = group(x -> agenttype(event_trial(window_data_event(x)).condition), WC[idxs])
     dct = Dict([(Object, "Object"), (Rat, "Rat"), (Robot, "Robot")])
+    dcc = Dict([(Object, :red), (Rat, :green), (Robot, :blue)])
 
 
-    f = Figure()
-    ax = Axis(f[1, 1], yscale = log10, xscale = log10, 
-        yminorticksvisible = true, yminorgridvisible = true,
-        yminorticks = IntervalsBetween(8))
+
+    # f = Figure()
+    # ax = Axis(f[1, 1], xscale = log10, yscale = log10,
+    #     yminorticksvisible = true, yminorgridvisible = true,
+    #     yminorticks = IntervalsBetween(8))
 
     for i in collect(1:1:length(g))
         k = collect(keys(g))[i]
         spcs = g[k] |> x -> wd_peri.(x) |> x -> peri_spec.(x)
         fl = spcs[1].flabels .* 1010.1 / 256
-        spcs = map(x -> x.y, spcs) |> x -> hcat(x...)
+        spcs = map(x -> x.y ./ sum(x.y), spcs) |> x -> hcat(x...)
         m = mean(spcs, dims = 2)
         s = std(spcs, dims = 2) / sqrt(size(spcs, 2))
         # errorbars!(ax, fl, vec(m), vec(s); linewidth = 2, label = k)
-        lines!(fl, vec(m), label = dct[k]; linewidth = 2)
-
+        lines!(ax, fl, vec(m), label = dct[k], color = dcc[k]; linewidth = 2)
+        band!(ax, fl, vec(m) - vec(s), vec(m) + vec(s), color = (dcc[k], 0.3))
+        xlims!(ax, fl[1], 120)
     end
-    xlims!(fl[1],120)
-    f[1, 2] = Legend(f, ax, "Agent")
-    ax.xlabel = "Freq (Hz)"
-    ax.ylabel = "Amp"
-    ax.title = "Condition:$trial_condition, Event:$event_name, Region:$(region.name)"
-    save("./Mean specs/$(trial_condition)_$(event_name)_$(region.name).png", f)
-
+    # fl = spcs[1].flabels .* 1010.1 / 256
+    # xlims!(fl[1], 120)
+    # f[1, 2] = Legend(f, ax, "Agent")
+    # ax.xlabel = "Freq (Hz)"
+    # ax.ylabel = "Amp"
+    ax.title = "$event_name, $(dcn[region])"
+    # save("./TempData/MeanSpecs/$(trial_condition)_$(event_name)_$(region.name).png", f)
+    # f
 end
 
-for e in ["Groom", "Rear", "Baseline", "Immobility"]
+dcn = Dict([(AMG, "Amygdala"), (MOB, "MOB"), (CA2, "CA")])
+
+f = Figure();
+axs = [Axis(f[i, j], xscale = log10, yscale = log10, yminorticksvisible = true, yminorgridvisible = true, yminorticks = IntervalsBetween(8)) for i = 1:3, j = 1:3]
+cnt = 1
+for e in ["Grooming", "Rearing", "Immobility"]
     for tc in ["Free Roam"]
         for r in [MOB, CA2, AMG]
-            fuckyouplot(e, tc, r)
+            fuckyouplot(axs[cnt], e, tc, r)
+            # if r == MOB
+            #     axs[cnt].title = "\b$e"
+            # else
+            #     # axs[cnt].title = "$(dcn[r])"
+            # end
+            # axs[cnt].title = "$(dcn[r]), $e"
+            # if e == "Grooming"
+            #     axs[cnt].ylabel = "Amp"
+            # end
+            # if r == AMG
+            #     axs[cnt].xlabel = "Freq (Hz)"
+            # end
+            cnt = cnt + 1
         end
     end
 end
 
+f
+# save("./TempData/MeanSpecs/allspecs.png", f)
 ##==============================================================================
 # f = Figure();
 # ax = Axis(f[1, 1], yscale = log10,
